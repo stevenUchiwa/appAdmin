f5327a1e5689303d7ae1b42362f866b40412d0d1
simeone <razanajatovo.simeone@gmail.com>
2021-10-21 12:00:47 +0300

    add gitignore

--- Serveur/API/dist/app.js
+++ Serveur/API/dist/app.js
@@ -4,12 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const port = 3001;
+const mongoURI = process.env.MONGO_URI;
 app.get('/', (req, res) => {
     res.send('The sedulous hyena ate the antelope!');
 });
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
 });
+//Conexion mongo
+mongoose_1.default
+    .connect(mongoURI)
+    .then(() => {
+    console.log("MongoDB Connected");
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=app.js.map
--- Serveur/API/src/app.ts
+++ Serveur/API/src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import mongoose from "mongoose";
+
 
 const app = express();
 const port = 3001;
+const mongoURI: string = process.env.MONGO_URI;
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
+
+//Conexion mongo
+
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
