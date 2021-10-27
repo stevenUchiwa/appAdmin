28bd984c6b0990f7a823894f1a2f383afa9d1112
simeone <razanajatovo.simeone@gmail.com>
2021-10-27 17:11:47 +0300

    add login form

--- front/src/component/Login/Login.tsx
+++ front/src/component/Login/Login.tsx
@@ -3,13 +3,16 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 import useStyles from './LoginStyle';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   }));
 
 
@@ -17,9 +20,18 @@ const Login = () => {
     const classes = useStyles()
     return(
         <Box sx={{ flexGrow: 1 }} className={classes.root}>
-        <Grid container spacing={2} justifyContent='center' alignContent='center' flexDirection='row'>
-          <Grid item xs={8}>
-            <Item>etest</Item>
+        <Grid container spacing={2} className={classes.gridContainerHeight} sx={{ justifyContent:'center',alignContent:'center'}} >
+          <Grid item xs={4}>
+            <Item>
+                <Typography>Login</Typography>
+                <div>
+                   <TextField id="outlined-basic" label="user name or mail" variant="outlined"/>
+                </div>
+                <div>
+                   <TextField id="outlined-basic" label="password" variant="outlined" type='password'/>
+                </div>
+                <Button variant="contained">Login</Button>
+            </Item>
           </Grid>
         </Grid>
       </Box>
